fbae5ac3445468254fda8b3a0abb2bb3e28de234
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-04-02 14:23:07 -0700

    added sessions, added db connection

--- server/index.js
+++ server/index.js
@@ -8,4 +8,22 @@ const app = express();
 
 app.use(bodyParser.json());
 
+
+//database connection
+massive(process.env.CONNECTION_STRING).then(db => {
+    console.log('database connected')
+    app.set('db', db)
+})
+
+//session connection
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+        // 2 weeks
+        maxAge: 60 * 60 * 24 * 14 * 1000
+    } 
+}))
+
 app.listen(4000, ()=> console.log('listening on port 4000'))
