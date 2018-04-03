97b1da917f3b4f3b70f28ef1aa4bf5e9197ded80
Anna Bender <annaelizabeth23@gmail.com>
2018-04-03 16:19:06 -0700

    added content and styles to home view

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Switch} from 'react-router-dom';
 import Header from './Components/Shared/Header';
+import Home from './Components/Home/Home';
 
 export default (
     
     <Switch>
-        <Route exact path='/' component={Header}/>
+        <Route exact path='/' component={Home}/>
         {/* <Route path='/customer' component={Customer}/>
         <Route path='/restaurant' component={Restaurant}/> */}
     </Switch>
