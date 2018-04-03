82fc3eccbeaacc1d314850ae00173f00be9b579a
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-04-02 17:04:58 -0700

    fixed app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './reset.css'
-import Modal from './components/login/Modal'
 import './App.css';
 
 
@@ -8,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="main">
-        <Modal/>
+        
       </div>
     );
   }
