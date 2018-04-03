f6ab04d40690d777440795d9b29a4aa8a7d2ecf1
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-04-02 21:10:45 -0700

    initial login modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import LoginModal from './Components/Login/LoginModal'
 import './reset.css'
 import './App.css';
 
@@ -7,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="main">
-        
+        <LoginModal/>
       </div>
     );
   }
