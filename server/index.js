f86007ec9c46c452f7721360ccbbe54bd53819a2
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-04-02 17:03:33 -0700

    added the material-ui package and necessary imports

--- server/index.js
+++ server/index.js
@@ -26,4 +26,6 @@ app.use(session({
     } 
 }))
 
+
+
 app.listen(4000, ()=> console.log('listening on port 4000'))
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './reset.css'
+import Modal from './components/login/Modal'
 import './App.css';
 
 
@@ -7,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="main">
-        
+        <Modal/>
       </div>
     );
   }
