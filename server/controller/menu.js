18f6994330a760730a4b21302f9f0d3e4eaacd7c
unknown <jordanmbush@gmail.com>
2018-04-03 18:58:25 -0700

    no change

--- server/controller/menu.js
+++ server/controller/menu.js
@@ -1,8 +1,9 @@
 module.exports = {
   getMenuItems: (req, res) => {
+    
     const id = req.params.id;
+    
     const db = req.app.get('db');
-
     db.get_menu_items(id).then( items => {
       res.json(items);
     }).catch( err => {
