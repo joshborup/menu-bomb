6f246225bb0de6e5a88ed5658e376132a583ae53
unknown <jordanmbush@gmail.com>
2018-04-03 19:20:08 -0700

    fixed get_menu_items.sql to use aliases for some fields to avoid ambiguity

--- db/get_menu_items.sql
+++ db/get_menu_items.sql
@@ -1,3 +1,3 @@
-SELECT * FROM menu_items M
+SELECT M.id as id, M.restaurant_id, name, price, description, image_url, C.id as category_id, C.category FROM menu_items M
 FULL JOIN categories C ON M.category_id = C.id
 WHERE M.restaurant_id = $1;
