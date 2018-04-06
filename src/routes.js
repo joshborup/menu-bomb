4de7a9e51af4325b092643ec90f331496ba02e0d
Anna Bender <annaelizabeth23@gmail.com>
2018-04-06 09:18:19 -0700

    added route

--- src/routes.js
+++ src/routes.js
@@ -5,14 +5,16 @@ import Home from './Components/Home/Home';
 import OrdersContainer from './Components/Customer/Orders/OrdersContainer'
 import testMenu from './Components/Menu/MenuContainer';
 import AccountContainer from './Components/Customer/Account/AccountContainer'
+import RestaurantDashboard from './Components/RestaurantDashboard/RestaurantDashboard';
 
 export default (
     
     <Switch>
-        <Route exact path='/' component={Home}/>
+        <Route exact path='/' component={Home} />
         <Route path='/customer/orders' component={OrdersContainer} />
         <Route path='/customer/account' component={AccountContainer} />
         <Route path='/testMenu/:id' component={testMenu} />
+        <Route path='/restaurantdashboard' component={RestaurantDashboard} />
     </Switch>
 
 )
