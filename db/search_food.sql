7cb2086f60d52700efc5958f1741272f9b727b33
unknown <jordanmbush@gmail.com>
2018-04-19 15:31:39 -0700

    modified query to exclude deleted menu items, and items of a deleted category

--- db/search_food.sql
+++ db/search_food.sql
@@ -1,7 +1,9 @@
 SELECT M.*, R.name as restaurant_name FROM menu_items M
 JOIN restaurant_profiles R
 ON (M.restaurant_id = R.id)
-WHERE M.name ILIKE CONCAT('%', $1 , '%')
+WHERE M.deleted = false
+AND (SELECT deleted FROM categories WHERE id = M.category_id) = false
+AND (M.name ILIKE CONCAT('%', $1 , '%')
 OR R.name ILIKE CONCAT('%', $1 , '%')
-OR M.description ILIKE CONCAT('%', $1 , '%')
+OR M.description ILIKE CONCAT('%', $1 , '%'))
 LIMIT 20;
